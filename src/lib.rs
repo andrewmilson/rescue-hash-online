62000c7fc399c50012d8025b1ca2fae49715170c
Andrew Milson <andrew.j.milson@gmail.com>
2022-05-18 18:50:21 +1000

    Remove unnecessary files

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,13 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
+// an allocator optimized for small code size
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
